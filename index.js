a3de809c49a206da854564a06f25919a58f9f47b
MuhammadAmin04 <13muhammadamin0804@gmail.com>
2023-06-13 07:42:46 +0500

    js

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+// function func(array) {
+//      console.log(array);
+//      let array = [1, 2, 3, 4, 5];
+//      let newArray = [];
+//      return array;
+// }
+
+
+//  map
+
+
+
+// deep clone
+
+// let arr=array.map((e,i,arr)=>{
+//      return {key:e}
+// })
+
+// let array = [1,2,3,4,5,6]
+
+// const juftRaqam=array.filter((e)=>{
+//     return e%2==0
+// })
+
+// console.log(array);
+// console.log(juftRaqam);
+
+
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// const result = words.filter(word => word.length > 6);
+
+// console.log(result);
+// // Expected output: Array ["exuberant", "destruction", "present"]
+
+
+// let students = [
+//   { name: "Ali", grade: 7, age: 16, surname: "Aliyev" },
+//   { name: "Vali", grade: 8, age: 19, surname: "Valiyev" },
+//   { name: "Gani", grade: 9, age: 17, surname: "Ganiyev" },
+//   { name: "Eshmat", grade: 10, age: 22, surname: "Eshmatov" },
+//   { name: "Toshmat", grade: 5, age: 16, surname: "Toshmatov" },
+// ];
+
+// function filterStudents(arr,input,key){
+//     let result = students.filter(students => students.name.includes('li') );
+//     console.log(result);
+// }
+
+// filterStudents(students,"G","surname");
+
+
+// let students = [
+//   { name: "Ali", grade: 7, age: 16, surname: "Aliyev" },
+//   { name: "Vali", grade: 8, age: 19, surname: "Valiyev" },
+//   { name: "Gani", grade: 9, age: 17, surname: "Ganiyev" },
+//   { name: "Eshmat", grade: 10, age: 22, surname: "Eshmatov" },
+//   { name: "Toshmat", grade: 5, age: 16, surname: "Toshmatov" },
+// ];
+
+// function filterStudents(arr, input, key) {
+//   let result = students.filter((students) => students.grade > 9);
+//   console.log(result);
+// }
+
+// filterStudents(students, "G", "surname");  
+
+
+
